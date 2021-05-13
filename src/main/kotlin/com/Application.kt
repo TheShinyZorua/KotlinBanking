799934ce78c351fcdc4a4388f7b3483749dea5f5
Roman <romananddayne@gmail.com>
2021-05-13 17:45:18 -0500

    Fixed kotlin config issue and added banking interface.

--- src/main/kotlin/com/Application.kt
+++ src/main/kotlin/com/Application.kt
@@ -10,4 +10,19 @@ fun main(args: Array<String>) {
 		.start()
 
 }
+fun main(args: Array<String>){
+	println("Welcome to your personal banking application.")
 
+	val bankaccount: BankAccount = BankAccount()
+	var usersChoice: Int = 1
+	while(usersChoice !=3){
+		choices()
+		display()
+	}
+}
+fun choices(){
+	println("What would you like to do today?")
+	println("1: Withdraw")
+	println("2: Deposit")
+	println("3: Exit")
+}
